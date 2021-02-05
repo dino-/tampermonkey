7dcfc883a5e3c292868dc56faa5ca504093f1528
Dino Morelli <dino@ui3.info>
2021-02-05 10:55:28 -0500

    Added support for unknown DRM state
    This code now does more sophisticated checks and can come to the
    conclusion that the DRM status is unknown. The search results are marked
    up different for this case (orange text, unknown message).

--- kobo-drm-in-search.user.js
+++ kobo-drm-in-search.user.js
@@ -12,7 +12,14 @@
 (function() {
   'use strict';
 
-  // log('script running');
+  // The values are unused, could be helpful for debugging
+  const drmStatus = {
+    NO: "does not have DRM",
+    YES: "has DRM",
+    UNKNOWN: "DRM status unknown"
+  }
+
+  // log('script started');
 
   // Get the anchor tag for every book on this search results page.
   // This URL drills down into the book details.
@@ -20,18 +27,39 @@
 
   books.each((_i, e) => visitBook(e));
 
+  log('script ended');
+
   function visitBook(bookElem) {
+    // Color the book gray before the (possibly lenghty) check
+    $(bookElem).css("color", "gray");
+
     // Retrieve the page contents
     var detailUrl = $(bookElem).attr('href');
     $.get(detailUrl, function(pageContents) {
-      // Search it for indications it has DRM
-      if (! /DRM-Free/.test(pageContents)) {
-        $(bookElem)
-          .css("color", "red")
-          .css("text-decoration", "line-through");
-        $(bookElem).parent().append("<span> has DRM!</span>");
+      switch (checkForDRM(pageContents)) {
+        case drmStatus.NO:
+          $(bookElem).css("color", "black");
+          break;
+        case drmStatus.YES:
+          $(bookElem)
+            .css("color", "red")
+            .css("text-decoration", "line-through");
+          $(bookElem).parent().append("<span> (has DRM!)</span>");
+          break;
+        case drmStatus.UNKNOWN:
+        default:
+          $(bookElem).css("color", "orange")
+          $(bookElem).parent().append("<span> (DRM status unknown)</span>");
       }
     });
+  }
+
+  // Returns what we can determine about the DRM. Yes, no or unknown using the above "enum"
+  function checkForDRM(pageContents) {
+    if (/DRM-Free/.test(pageContents)) { return drmStatus.NO; }
+    else if (/sold without Digital Rights Management Software/.test(pageContents)) { return drmStatus.NO; }
+    else if (/Adobe DRM/.test(pageContents)) { return drmStatus.YES; }
+    else { return drmStatus.UNKNOWN; }
   }
 
   // Convenience function. Shorter to call and can be filtered in the console easily.
